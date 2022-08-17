db09395a6be969fd900c73db93c119a63839b2ff
Starniraj <singhniraj637@gmail.com>
2022-08-17 15:48:19 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import {BrowserRouter, Routes,Route} from "react-router-dom"
+import Header from './component/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     
+    </div>
+  );
+}
+
+export default App;
